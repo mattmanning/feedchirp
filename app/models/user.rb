b3cd370b509e0d19525acf405a4d47e4676ed2b5
Matt Manning <matt.manning@gmail.com>
2014-02-04 22:53:24 -0500

    Remove devise.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,2 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :rememberable, :trackable, :omniauthable,
-         :omniauth_providers => [:twitter]
 end
