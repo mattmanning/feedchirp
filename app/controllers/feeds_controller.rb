41ffa0bc4480adb9d367876ac3022f21b1a39643
Matt Manning <matt.manning@gmail.com>
2014-02-07 03:08:57 -0500

    Get index scaffold page working.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,7 +4,7 @@ class FeedsController < ApplicationController
   # GET /feeds
   # GET /feeds.json
   def index
-    @feeds = Feed.all
+    @user_feeds = UserFeed.find_all_by_user_id current_user.id
   end
 
   # GET /feeds/1
