69f17447012e9a76442547497eca07bd23bfa34c
Matt Manning <matt.manning@gmail.com>
2014-02-17 00:08:17 -0500

    Change 'Destroy' to 'Delete' and disable the show action for now.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -10,8 +10,9 @@ class FeedsController < ApplicationController
   # GET /feeds/1
   # GET /feeds/1.json
   def show
-    @feed = Feed.find(params[:id])
-    @user_feed = current_user.user_feeds.find_by_feed_id(@feed.id)
+    redirect_to @feeds
+    # @feed = Feed.find(params[:id])
+    # @user_feed = current_user.user_feeds.find_by_feed_id(@feed.id)
   end
 
   # GET /feeds/new
