8270287eb6d663a5533f4396c33cd72b45ff5b22
Matt Manning <matt.manning@gmail.com>
2014-02-17 01:46:05 -0500

    More renaming

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rsstweet::Application.configure do
+Feedchirp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
