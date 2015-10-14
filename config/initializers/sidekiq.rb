e4180bf7677970442d1ea5279c5ffc21b8866bdf
Matthew Manning <matt.manning@gmail.com>
2015-10-14 04:05:17 -0400

    fix initializers

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,11 +3,3 @@ require 'sidekiq/web'
 Sidekiq::Web.use Rack::Auth::Basic do |username, password|
   username == ENV['SIDEKIQ_USERNAME'] && password == ENV['SIDEKIQ_PASSWORD']
 end
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://:#{ENV['REDIS_ENV_REDIS_PASSWORD']}@#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}/#{ENV['REDIS_ENV_REDIS_DATABASE']}" }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://:#{ENV['REDIS_ENV_REDIS_PASSWORD']}@#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}/#{ENV['REDIS_ENV_REDIS_DATABASE']}" }
-end
