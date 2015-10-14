6ffb77647480f00f7aa3d8d3c5788da4481abc2f
Matthew Manning <matt.manning@gmail.com>
2015-10-14 04:25:27 -0400

    retry logic

--- lib/clock.rb
+++ lib/clock.rb
@@ -5,7 +5,11 @@ require 'clockwork'
 include Clockwork
 
 every(10.minutes, 'Running FeedUpdater') do
-  Feed.all.each do |feed|
-    FeedUpdater.perform_async(feed.id)
+  begin
+    Feed.all.each do |feed|
+      FeedUpdater.perform_async(feed.id)
+    end
+  rescue PG::UndefinedTable
+    retry
   end
 end
